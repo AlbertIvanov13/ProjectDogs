f0265ea967078fdd2106bd670dc810920d729a02
AlbertIvanov13 <albertivanov187@gmail.com>
2022-02-08 09:47:12 +0200

    Upload project

---
+++ 2.DogAppFull-CRUD/2.DogAppFull-CRUD-без сървиси/DogApp/Abstractions/IDogService.cs
@@ -0,0 +1,39 @@
+﻿using DogApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogApp.Abstractions
+{
+    public interface IDogService
+    {
+        bool Create(string name, int age, string breed, string picture);
+
+        bool UpdateDog(int dogId, string name, int age, string breed, string picture);
+
+        List<Dog> GetDogs();
+
+        public List<Dog> GetDogs(string searchStringBreed, string searchStringName)
+        {
+            List<Dog> dogs = _context.Dogs.ToList();
+            if (!String.IsNullOrEmpty(searchStringBreed) && !String.IsNullOrEmpty(searchStringName))
+            {
+                dogs = dogs.Where(d => d.Breed.Contains(searchStringBreed) && d.Name.Contains(searchStringName)).ToList();
+            }
+            else if (!String.IsNullOrEmpty(searchStringBreed))
+            {
+                dogs = dogs.Where(d => d.Breed.Contains(searchStringBreed)).ToList();
+            }
+            else if (!String.IsNullOrEmpty(searchStringName))
+            {
+                dogs = dogs.Where(d => d.Name.Contains(searchStringName)).ToList();
+            }
+
+            return dogs;
+        }
+
+        Dog GetDogById(int dogId);
+        bool RemoveById(int dogId);
+    }
+}
